d62a7cf6a2eaf60bda1d37ed4c3a8b0b600f1e44
Mitchel Parish <mitchelparish@gmail.com>
2019-02-18 17:46:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,11 +20,9 @@ class Dog
     DB[:conn].execute(sql)
   end 
 
-
-
-def self.drop_table
-  DB[:conn].execute('DROP TABLE dogs')
-end
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE dogs')
+  end
 
 
 
