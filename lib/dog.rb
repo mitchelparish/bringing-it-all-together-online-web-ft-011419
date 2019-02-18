2154f956b3c334943ff858f4ff95ee05cf06f5ab
Mitchel Parish <mitchelparish@gmail.com>
2019-02-18 17:52:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,13 +36,13 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    self 
+    self
   end
 
   def self.create(name:, breed:)
     dog = Dog.new(name, breed)
     dog.save
-    dog 
+    dog
   end 
 
 
